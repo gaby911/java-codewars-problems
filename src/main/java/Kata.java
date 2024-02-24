3e130695be29769b48c4906c4a2c48c42d89e785
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-24 17:15:24 +0200

    Even or Odd

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -12,4 +12,17 @@ public class Kata {
     public static int opposite(int number) {
         return 0 - number;
     }
+
+    /*
+    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+    */
+
+    public static String even_or_odd(int number) {
+        if(number%2 == 0)
+            return "Even";
+        else
+            return "Odd";
+    }
+
+
 }
