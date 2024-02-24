3a2502c03802da40dff75d9ed42423618116fa76
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-24 17:20:19 +0200

    Sum of positive

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -49,4 +49,24 @@ public class Kata {
 
     }
 
+    /*
+    DESCRIPTION:
+        You get an array of numbers, return the sum of all of the positives ones.
+
+        Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+        Note: if there is nothing to sum, the sum is default to 0.
+     */
+
+    public static int sum(int[] arr) {
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
 }
