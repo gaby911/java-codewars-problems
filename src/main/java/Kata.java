ba77e869e073a2e14dfafe1c2725e34a61b9e890
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-24 21:42:04 +0200

    counting sheep

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -196,5 +196,33 @@ public class Kata {
         return new StringBuffer(str).deleteCharAt(str.length() - 1).deleteCharAt(0).toString();
     }
 
+    /*
+    DESCRIPTION:
+    Consider an array/list of sheep where some sheep may be missing from their place.
+    We need a function that counts the number of sheep present in the array (true means present).
+    For example,
+
+    [true,  true,  true,  false,
+      true,  true,  true,  true ,
+      true,  false, true,  false,
+      true,  false, false, true ,
+      true,  true,  true,  true ,
+      false, false, true,  true]
+    The correct answer would be 17.
+
+    Hint: Don't forget to check for bad values like null/undefined
+    */
+
+    public int countSheeps(Boolean[] arrayOfSheeps) {
+        int counter = 0;
+        for (Boolean sheep : arrayOfSheeps) {
+            if (sheep != null) {
+                if (sheep) counter++;
+            }
+        }
+        return counter;
+    }
+
+
 
 }
