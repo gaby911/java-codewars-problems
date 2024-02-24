b14c67c1c3e9ead61e4f21e3a85e9fb9218c70d3
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-24 21:15:44 +0200

    String repeat

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -92,5 +92,39 @@ public class Kata {
             return -x;
     }
 
+    /*
+    DESCRIPTION:
+        Write a function that accepts an integer n and a string s as parameters,
+        and returns a string of s repeated exactly n times.
+
+        Examples (input -> output)
+        6, "I"     -> "IIIIII"
+        5, "Hello" -> "HelloHelloHelloHelloHello"
+        */
+
+    public static String repeatStr(final int repeat, final String string) {
+
+        String s = "";
+        for (int i = 0; i < repeat; i++) s += string;
+        return s;
+    }
+
+    public static String repeatStrv2(final int repeat, final String string) {
+
+        if (repeat == 0)
+            return "";
+        else
+            return string.repeat(repeat);
+    }
+
+    public static String repeatStrv3(final int repeat, final String string) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < repeat; i++) {
+            sb.append(string);
+        }
+        return String.valueOf(sb);
+    }
+
 
 }
