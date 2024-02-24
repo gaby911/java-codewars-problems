34dc6d3dbcb66f67cd29cf5383d38c368b62e20e
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-24 17:18:29 +0200

    Find the smallest integer in the array

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -18,11 +18,35 @@ public class Kata {
     */
 
     public static String even_or_odd(int number) {
-        if(number%2 == 0)
+        if (number % 2 == 0)
             return "Even";
         else
             return "Odd";
     }
 
+    /*
+    DESCRIPTION:
+        Given an array of integers your solution should find the smallest integer.
+
+        For example:
+
+        Given [34, 15, 88, 2] your solution will return 2
+        Given [34, -345, -1, 100] your solution will return -345
+        You can assume, for the purpose of this kata, that the supplied array will not be empty.
+     */
+
+
+    public static int findSmallestInt(int[] args) {
+        int smallestNumber = args[0];
+
+        for (int i = 1; i < args.length; i++) {
+
+            if (args[i] < smallestNumber) {
+                smallestNumber = args[i];
+            }
+        }
+        return smallestNumber;
+
+    }
 
 }
