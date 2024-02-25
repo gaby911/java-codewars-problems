ca640f87ba48097dcc2729b10aa73b3e138d712a
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-25 15:43:13 +0200

    Jenny's secret message

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -290,5 +290,19 @@ public class Kata {
         }
     }
 
+    /*
+    DESCRIPTION:
+    Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny,
+     and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
 
+    Can you help her?
+    */
+
+    public static String greet(String name) {
+        if (name.equals("Johnny")) {
+            return "Hello, my love!";
+        } else
+            return String.format("Hello, %s!", name);
+
+    }
 }
