f18a546000bdd8505692f9c0c4a5fe7ed9dba837
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-25 15:19:34 +0200

    Reversed Strings

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -239,5 +239,26 @@ public class Kata {
         return x.trim().replace(" ", "");
     }
 
+    /*
+    DESCRIPTION:
+    Complete the solution so that it reverses the string passed into it.
+
+    'world'  =>  'dlrow'
+    'word'   =>  'drow'
+    */
+
+    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static String solution2(String str) {
+        String res = "";
+        for (int i = 1; i <= str.length(); i++) {
+            res += str.charAt(str.length() - i);
+        }
+        return res;
+    }
 
 }
