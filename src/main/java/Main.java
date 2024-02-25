6e83ba80e930914e7289f453da5116cbc941bab2
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-02-25 16:00:17 +0200

    Square(n) Sum

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Array;
+
+public class Main {
+    public static void main(String[] args) {
+//        Kata.squareSum(new int[]{1, 2, 2});
+//        System.out.println(Kata.squareSum(new int[]{1, 2, 2}));
+
+
+    }
+}
